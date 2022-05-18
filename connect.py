33ca73081c4a494271c0274a992cdf221c3870cf
FLG <90811740+FranzLouiseGloriani@users.noreply.github.com>
2022-05-18 19:36:07 +0800

    Delete connect.py

--- connect.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-try:
-    connection = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=C:\Users\sayom\OneDrive\Documents\Database1.accdb;')
-    print("Database is connected")
-
-except pyodbc.Error:
-    print("Database is NOT connected")
